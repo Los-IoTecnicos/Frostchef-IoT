01d6530a99261ef0ca2e5f32967da04c90321715
Prog-san <DieSanR@gmail.com>
2024-11-01 20:35:04 -0500

    feat: Add methane gas sensor & proximity sensor
    Se añaden las funciones del detector de metano MQ2 y el sensor de proximidad HC-SR04, junto a su visualización mediante el lector LCD
    
    Co-Authored-By: Yordi  <104015456+YordiDG@users.noreply.github.com>
    Co-Authored-By: Jose110903 <103531140+Jose110903@users.noreply.github.com>

--- project/mq2-gas-sensor.chip.c
+++ project/mq2-gas-sensor.chip.c
@@ -9,15 +9,65 @@
 #include <stdlib.h>
 
 typedef struct {
-  // TODO: Put your chip variables here
-  float phValue;
 
+  //Pines
+  pin_t pin_a0;
+  pin_t pin_d0;
+  pin_t pin_vcc;
+  pin_t pin_gnd;
+  //Methane Gas Attribute
+  uint32_t methane_attr;
+  uint32_t threshold_attr;
 } chip_state_t;
+
+// Pre-declare of timer Event Handler
+static void chip_timer_event(void *user_data);
+
 
 void chip_init() {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
 
   // TODO: Initialize the chip, set up IO pins, create timers, etc.
 
+  // Pin Setup
+  chip->pin_a0  = pin_init("A0", ANALOG);
+  chip->pin_d0  = pin_init("D0", OUTPUT_LOW);
+  chip->pin_vcc = pin_init("VCC", INPUT_PULLDOWN);
+  chip->pin_gnd = pin_init("GND", INPUT_PULLUP);
+
+  // Attributes Setup
+  chip->methane_attr = attr_init("Methane", 1);
+  chip->threshold_attr  = attr_init("threshold", 50);
+  // Timer Event Configuration
+  const timer_config_t timer_config = {
+    .callback = chip_timer_event,
+    .user_data = chip
+  };
+
+  // Timer Initialization and Start
+  timer_t timer_id = timer_init(&timer_config);
+  timer_start(timer_id, 1000, true);
+
+
+
   printf("Hello from custom chip!\n");
+}
+
+// Timer Event Handler Implementation
+void chip_timer_event(void *user_data) {
+  // Get Event Payload
+  chip_state_t *chip = (chip_state_t*)user_data;
+  // Calculate Voltage
+  float voltage = (attr_read_float(chip->methane_attr))*5.0/100;
+  // Calculate Threshold
+  float threshold_v = (attr_read_float(chip->threshold_attr))*5.0/100;
+  // Check if operating
+  if (pin_read(chip->pin_vcc) && !pin_read(chip->pin_gnd)) {
+    pin_dac_write(chip->pin_a0, voltage);
+    // Check if Voltage Value exceeds Threshold Limit
+    if(voltage > threshold_v)
+      pin_write(chip->pin_d0, HIGH);
+    else
+      pin_write(chip->pin_d0, LOW);
+  }
 }
--- project/sketch.ino
+++ project/sketch.ino
@@ -1,18 +1,112 @@
 // Incluye la biblioteca para ESP32
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <MQUnifiedsensor.h>
+
+#include "mq2-setup.h"
+
+// MQ2 Sensor Attributes
+#define Board               ("ESP32")
+#define Pin                 (4)
+#define Threshold           (23)
+#define Type                ("MQ-2")
+#define Voltage_Resolution  (3.3)
+#define ADC_Bit_Resolution  (12)
+#define RatioMQ2CleanAir    (9.83)
+
+// MQ2 Library Instance Setup
+MQUnifiedsensor MQ2(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin, Type);
+
+// Display Setup
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
 
 // Define el pin al que está conectado el sensor de proximidad
 const int sensorPin = 2; // Cambia el número del pin según tu conexión
 
+// Variables
+int gas_value = 0;
+
+
+
 void setup() {
-  // Inicializa el monitor serial
+
+  // LCD Screen Initialization
+  lcd.init();
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("On Line");
+  delay(1000);
+
+  // Serial Port Communication Initialization
   Serial.begin(9600);
+  // Parameter Setup to detect PPM concentration for LPG
+  MQ2.setRegressionMethod(1);
+  MQ2.setA(574.25);
+  MQ2.setB(-2.222);
+
+  // MQ2 Initialization
+  MQ2.init();
+
+  // Calibrate Sensor
+  Serial.print("Calibrating Sensor, please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i <= 10; i++) {
+    MQ2.update();
+    calcR0 = MQ2.calibrate(RatioMQ2CleanAir);
+    Serial.print(".");
+  }
+
+  // Update Calibration Factor
+  MQ2.setR0(calcR0/10);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("MQ2 = ");
+  lcd.print(calcR0);
+  delay(500);
+
+  // Calibration Exception Handling
+  if(isinf(calcR0)) { Serial.print("Warning: R0 value is infinite. Please check your wiring"); while(1); }
+  if(calcR0 == 0) { Serial.print("Warning: R0 value is zero. Please check your wiring"); while(1); }
+
+  // Show MQ2 Debug Log
+  MQ2.serialDebug(true);
 
   // Configura el sensorPin como entrada
-  pinMode(sensorPin, INPUT);
+  //pinMode(sensorPin, INPUT);
+  pinMode(Threshold, INPUT);
 }
 
 void loop() {
+
+  // Update Reading
+  MQ2.update();
+  MQ2.readSensor();
+  MQ2.serialDebug();
+
+  // Read Values
+  gas_value = analogRead(Pin);
+  gas_value = map(gas_value, 0, 4095, 0, 100);
+  Serial.println(gas_value);
+
+  // Show value in display
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("MQ2 = ");
+  lcd.print(gas_value);
+  lcd.print("%");
+
+  // Show Threshold Indicator
+  lcd.setCursor(0, 1);
+  if (digitalRead(Threshold) == HIGH) {
+    lcd.print("Unsafe");
+  } else {
+    lcd.print("Safe");
+  }
+
   // Lee el valor del sensor de proximidad
   int sensorValue = digitalRead(sensorPin);
 
@@ -21,5 +115,5 @@ void loop() {
   Serial.println(sensorValue);
 
   // Espera un breve período de tiempo antes de volver a leer
-  delay(1000);
+  delay(2000);
 }
